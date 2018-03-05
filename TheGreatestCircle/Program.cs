2e4e24c4f67670e9c76de65a843cf45062eec4be
aaronshortt <37086802+aaronshortt@users.noreply.github.com>
2018-03-05 21:59:18 +0000

    FInished IO

--- TheGreatestCircle/Program.cs
+++ TheGreatestCircle/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,7 +11,40 @@ namespace TheGreatestCircle
     {
         static void Main(string[] args)
         {
+            var program = new Program();
 
+            //Read in Customers
+            var customrs = program.ReadCustomersFromFile();
+
+
+            
         }
+
+
+        private List<Customer> ReadCustomersFromFile()
+        {
+            List<Customer> customers = new List<Customer>();
+            try
+            {
+                using (StreamReader streamReader = new StreamReader("gistFile1.txt"))
+                {
+                    while (!streamReader.EndOfStream)
+                    {
+                        string json = streamReader.ReadLine();
+                        customers.Add(Customer.FromJson(json));
+                    }
+                    streamReader.Close();
+                }
+            }catch (IOException ex)
+            {
+                Console.WriteLine(ex.StackTrace);
+                throw new FileNotFoundException("Could not find Gist File Containing JSON");
+            }
+
+            return customers;
+        }
+
+
     }
+
 }
